38315373849815f080ecd805bc7f77a8286e705c
Atogb KMEX <atogb.kmex@outlook.fr>
2022-05-11 11:27:54 +0200

    mon dernier commit

---
+++ Form3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EcranDialogue
+{
+    public partial class Form3 : Form
+    {
+        string adhérent;
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            adhérent = "Nouveau client";
+            label1.Text = adhérent;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            adhérent = "Ancien client";
+            label1.Text = adhérent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ecran ecran = new ecran();
+            ecran.Show();
+            this.Hide();
+        }
+    }
+}
